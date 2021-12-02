4c7cf569c60abd96b3410fba67ece5d3d413cbe4
Guilherme Devólio <guinogaroto@gmail.com>
2021-12-02 12:01:31 -0300

    Cleaning callbacl

--- callback.php
+++ callback.php
@@ -1,17 +1,18 @@
 <?php
 session_start();
-
-require_once './Services/SpotifyService.php';
 header('Content-Type: application/json; charset=utf-8');
-
-$ss = new SpotifyService();
+require_once './Services/SpotifyService.php';
 
 $code = filter_input(INPUT_GET, 'code', FILTER_SANITIZE_STRIPPED);
 
 if(!$code) {
-    exit(json_encode(['status' => 'erro', 'mensagem' => 'O código de autorização não foi informado'], JSON_UNESCAPED_UNICODE));
+    exit(json_encode([
+        'status' => 'erro',
+        'mensagem' => 'O código de autorização não foi informado'
+    ], JSON_UNESCAPED_UNICODE));
 }
 
+$ss = new SpotifyService();
 $response = $ss->getAuthorizationToken($code);
 
 if(!$response['access_token'] || empty($response['access_token'])) {
